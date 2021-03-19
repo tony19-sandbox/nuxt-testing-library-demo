893b26ba77147a946b78af189f1d8e1229690c83
Tony Trinh <tony19@gmail.com>
2021-03-18 20:07:45 -0500

    add jest support

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'ts', // if using TypeScript
+    'js',
+    'vue',
+    'json'
+  ],
+  transform: {
+    "^.+\\.ts$": "ts-jest", // if using TypeScript
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ]
+}
